5911f3953d10dd5b2629157bdde1083fae5eba78
Matthew McFarland <mmcfarland@azavea.com>
2016-08-12 17:06:01 -0400

    Add simple /count endpoint
    Adds an endpoint at /count which accepts a file in a shared data dir and
    returns a summary of the frequency of cell values in a predetermined but
    arbitrary window.

--- src/geop/main.py
+++ src/geop/main.py
@@ -1,22 +1,60 @@
-from flask import Flask
-import rasterio
+import os
+import time
 
-import os.path
+from flask import Flask, request, jsonify
+import numpy as np
+import rasterio
 
 app = Flask(__name__)
 
-
-@app.route('/histogram', methods=['POST'])
-def histogram():
-    # Testing that the library is available
-    print os.path.isfile('/vagrant/shapes/DelDem4ad8.tif')
-    #src = rasterio.open('/vagrant/shapes/DelDem4ad8.tif')
-    #return src.read(1)
+DATA_PATH = '/usr/data/'
 
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return 'pong'
+@app.route('/counts', methods=['POST'])
+def count():
+    """
+    Perform a rudimentary cell count analysis on a portion of a
+    provided raster
+
+    Query String Args:
+        filename: file name of a valid geotiff in DATA_DIR
+    """
+
+    raster = request.args.get('filename')
+    if not raster:
+        return handle_error('filename parameter is required')
+
+    raster_path = os.path.join(DATA_PATH, raster)
+    if not os.path.isfile(raster_path):
+        return handle_error('{} is not valid file in DATA_DIR'.format(raster))
+
+    counts = {}
+    start = time.clock()
+    with rasterio.open(raster_path) as src:
+        w = src.read(1, window=((10000, 10350), (10000, 10350)))
+
+        for idx, cell in np.ndenumerate(w):
+            cell_val = str(cell)
+
+            if cell_val in counts:
+                counts[cell_val] += 1
+            else:
+                counts[cell_val] = 1
+
+    elapsed = time.clock() - start
+    cells = reduce(lambda s, key: s + counts[key], counts.keys(), 0)
+
+    return jsonify({
+        'time': elapsed,
+        'cellCount': cells,
+        'counts': counts,
+    })
+
+
+def handle_error(msg, code=400):
+    response = jsonify({'message': msg})
+    response.status_code = code
+    return response
 
 
 if __name__ == '__main__':
