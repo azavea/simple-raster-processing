d4e4305b9fe8197e296357f60e24e7f39017f2b5
Matthew McFarland <mmcfarland@gmail.com>
2016-08-21 16:14:25 -0400

    Extract vector masking function
    Extract the function which masks a reaster by a vector shape out to a
    well documented common utility function.

--- src/geop/main.py
+++ src/geop/main.py
@@ -1,13 +1,11 @@
 import time
 import numpy as np
-import rasterio
 
 from flask import Flask, request, jsonify
-from rasterio import features
 from shapely.geometry import shape
 
 from errors import UserInputError
-from geo_utils import get_window_and_affine, reproject
+from geo_utils import mask_geom_on_raster, reproject
 from request_utils import parse_config
 
 
@@ -29,27 +27,7 @@ def count():
 
     raster_path = user_input['raster_paths'][0]
 
-    with rasterio.open(raster_path) as src:
-        # Read a chunk of the raster that contains the bounding box of the
-        # input geometry.  This has memory implications if that rectangle
-        # is large. The affine transformation maps geom coordinates to the
-        # image mask below.
-        window, shifted_affine = get_window_and_affine(geom, src)
-        data = src.read(1, window=window)
-
-    # Create a numpy array to mask cells which don't intersect with the
-    # polygon. Cells that intersect will have value of 0 (unmasked), the
-    # rest are filled with 1s (masked)
-    geom_mask = features.rasterize(
-        [(geom, 0)],
-        out_shape=data.shape,
-        transform=shifted_affine,
-        fill=1,
-        dtype=np.uint8,
-        all_touched=True
-    )
-
-    masked_data = np.ma.array(data=data, mask=geom_mask.astype(bool))
+    masked_data = mask_geom_on_raster(geom, raster_path)
 
     # Perform count using numpy built-ins.  Compressing the masked array
     # creates a 1D array of just unmasked values.  May be able to speed up
@@ -69,7 +47,7 @@ def count():
 @app.route('/pair-counts', methods=['POST'])
 def pair_counts():
     """
-    For a pair of rasters whos extents and srs match, count cell pairs that
+    For a pair of rasters whose extents and srs match, count cell pairs that
     occur when the rasters are stacked.  This resembles the geoprocessing
     required to run TR-55.
     """
@@ -80,23 +58,8 @@ def pair_counts():
             shape(user_input['query_polygon']),
             to_srs=user_input['srs'])
 
-    def mask_geom_on_raster(raster_path):
-        with rasterio.open(raster_path) as src:
-            window, shifted_affine = get_window_and_affine(geom, src)
-            data = src.read(1, window=window)
-
-        geom_mask = features.rasterize(
-            [(geom, 0)],
-            out_shape=data.shape,
-            transform=shifted_affine,
-            fill=1,
-            dtype=np.uint8,
-            all_touched=True
-        )
-
-        return np.ma.array(data=data, mask=geom_mask.astype(bool))
-
-    layers = tuple(mask_geom_on_raster(raster_path)
+    # Read in two rasters and mask geom on both of them
+    layers = tuple(mask_geom_on_raster(geom, raster_path)
                    for raster_path in user_input['raster_paths'])
 
     # Take the two masked arrays, and stack them along the third axis
